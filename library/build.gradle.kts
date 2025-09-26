aeb71f9f288f2be2d4954d9c5382d465bbc81d65
Alex Dufournet <alex.dufournet@guardian.co.uk>
2025-09-26 17:22:06 +0200

    metalava

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.metalava)
     kotlin("plugin.serialization") version "1.9.0"
 }
 
