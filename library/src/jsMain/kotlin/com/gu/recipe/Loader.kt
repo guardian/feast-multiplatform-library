c8eff9265a416c47be970eea6ad018fee52a755d
Andy Gallagher <fredex42@gmail.com>
2026-03-06 21:07:17 +0000

    working in js

--- library/src/jsMain/kotlin/com/gu/recipe/Loader.kt
+++ library/src/jsMain/kotlin/com/gu/recipe/Loader.kt
@@ -1,14 +1,15 @@
 package com.gu.recipe
 
-import kotlinx.browser.window
-import org.w3c.fetch.RequestInit
+import org.w3c.fetch.Response
 import kotlinx.coroutines.await
 import org.w3c.fetch.Headers
 import kotlin.js.Promise
 
-private fun Headers.toMap(): Map<String, String> =
-    (js("Array.from(this.entries())") as Array<Array<String>>)
-        .associate { (key, value) -> key to value }
+private fun Headers.toMap(): Map<String, String>? {
+    val headers = this
+    return (js("headers && Array.from(headers.entries())") as Array<Array<String>>?)
+        ?.associate { (key, value) -> key to value }
+}
 
 fun Map<String, String>.toJsHeaders(): dynamic {
     val obj = js("({})")
@@ -16,21 +17,19 @@ fun Map<String, String>.toJsHeaders(): dynamic {
     return obj
 }
 
-private fun fetchJs(url: String, method: String, headers: dynamic, body: dynamic): dynamic {
+private fun fetchJs(url: String, method: String, headers: dynamic, body: dynamic): Promise<Response> {
     return js("fetch(url, { method: method, headers: headers, body: body })")
 }
 
 actual suspend fun makeHttpRequest(method: String, url: String, body: String?, headers:Map<String, String>?): Loader.HttpResponse {
     try {
-        val promise = fetchJs(url, method, headers?.toJsHeaders(), body).then { response ->
-            Loader.HttpResponse(
-                response.status.toInt(),
-                response.headers.toMap(),
-                response.body.decodeToString()
-            )
-        }
-
-        return promise.await()
+        val response = fetchJs(url, method, headers?.toJsHeaders(), body).await()
+        val content = response.text().await()
+        return Loader.HttpResponse(
+            response.status.toInt(),
+            response.headers.toMap(),
+            content
+        )
     } catch (e: Exception) {
         println("Error loading $url: ${e.message}")
         return Loader.HttpResponse(
