f3adc325f5424db98edd3c17fbd0770609af7ccf
Mohammad Haque <mohammad.haque@guardian.co.uk>
2026-04-28 16:14:50 +0100

    fix CVE-2026-33871

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply  false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
+
+
+// Fix CVE: Netty HTTP/2 CONTINUATION Frame Flood DoS - transitive via AGP 8.12.3 (netty 4.1.110.Final)
+allprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("io.netty:netty-codec-http2:4.1.132.Final")
+        }
+    }
+}
+
