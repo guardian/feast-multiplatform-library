4821cee4d7c2752beaa93b9323a64a8975a05d44
Andy Gallagher <fredex42@gmail.com>
2026-07-15 18:40:09 +0100

    add the query we built together and update schema to cover the newly added fields in https://github.com/guardian/feast-unified-api/pull/29

--- core/graphql/src/commonMain/kotlin/com/gu/recipe/core/graphql/repository/ApolloRecipeGraphQlDataSource.kt
+++ core/graphql/src/commonMain/kotlin/com/gu/recipe/core/graphql/repository/ApolloRecipeGraphQlDataSource.kt
@@ -3,6 +3,7 @@ package com.gu.recipe.core.graphql.repository
 import com.gu.recipe.core.graphql.GraphQlResult
 import com.gu.recipe.core.graphql.client.FeastGraphQlClient
 import com.gu.recipe.core.graphql.generated.GetFrontsByRegionQuery
+import com.gu.recipe.core.graphql.generated.CurationForTestQuery
 import com.gu.recipe.core.graphql.generated.type.Editions
 import com.gu.recipe.core.graphql.generated.type.Regions
 
@@ -21,6 +22,21 @@ class ApolloRecipeGraphQlDataSource(
             )
         )) {
             is GraphQlResult.Success -> GraphQlResult.Success(result.value.Front)
+            is GraphQlResult.Failure -> result
+        }
+    }
+
+    override suspend fun getCurationForTest(
+        region: Regions,
+        edition: Editions
+    ): GraphQlResult<List<CurationForTest.Front>> {
+        return when (val result = feastGraphQlClient.query(
+            CurationForTestQuery(
+                region = region,
+                edition = edition
+            )
+        )) {
+            is GraphQlResult.Success -> GraphQlResult.Success(result.value)
             is GraphQlResult.Failure -> result
         }
     }
