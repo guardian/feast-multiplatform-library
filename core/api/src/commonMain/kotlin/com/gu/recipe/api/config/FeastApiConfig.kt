dbeb77d8d70512cb39a423ad18143f98d5d96576
shamoon_ahmed <shamoon.ahmed@guardian.co.uk>
2026-07-23 09:07:53 +0100

    hidden networking layer

--- core/api/src/commonMain/kotlin/com/gu/recipe/api/config/FeastApiConfig.kt
+++ core/api/src/commonMain/kotlin/com/gu/recipe/api/config/FeastApiConfig.kt
@@ -1,16 +1,23 @@
 package com.gu.recipe.api.config
 
+import com.gu.recipe.api.model.FeastEnvironment
 import com.gu.recipe.core.graphql.config.GraphQlConfig
-import com.gu.recipe.core.graphql.provider.DefaultFeastGraphQlServerUrlProvider
-import com.gu.recipe.core.graphql.provider.FixedGraphQlServerUrlProvider
+import com.gu.recipe.core.networking.FeastNetworkApiEndpoint
 import com.gu.recipe.core.networking.NetworkConfig
 
 data class FeastApiConfig(
-    val networkConfig: NetworkConfig,
+    val environment: FeastEnvironment,
 )
 
-internal fun FeastApiConfig.toGraphQlConfig(): GraphQlConfig =
-    GraphQlConfig(
-        networkConfig = networkConfig,
-    )
+internal fun FeastApiConfig.toGraphQlConfig(): GraphQlConfig {
+    val endpoint = when (environment) {
+        FeastEnvironment.CODE -> FeastNetworkApiEndpoint.CODE
+        FeastEnvironment.PROD -> FeastNetworkApiEndpoint.PROD
+    }
 
+    val networkConfig = NetworkConfig(endpoint = endpoint)
+
+    return GraphQlConfig(
+        networkConfig = networkConfig
+    )
+}
