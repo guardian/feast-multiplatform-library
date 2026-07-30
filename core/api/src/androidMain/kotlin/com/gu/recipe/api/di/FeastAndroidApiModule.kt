6abe3b429eac8eb43f8cafc4d2dec4c18e43389e
Mohammad Haque <mohammad.haque@guardian.co.uk>
2026-07-30 18:50:30 +0100

    removed hilt,ksp,wrapper di

--- core/api/src/androidMain/kotlin/com/gu/recipe/api/di/FeastAndroidApiModule.kt
+++ core/api/src/androidMain/kotlin/com/gu/recipe/api/di/FeastAndroidApiModule.kt
@@ -3,8 +3,8 @@ package com.gu.recipe.api.di
 import android.content.Context
 import com.gu.recipe.api.repository.GraphQLRepository
 import com.gu.recipe.api.repository.GraphQlRepositoryImpl
-import com.gu.recipe.core.graphql.androidGraphQlModule
 import com.gu.recipe.core.graphql.config.GraphQlConfig
+import com.gu.recipe.core.graphql.di.graphQlModule
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.Module
@@ -14,15 +14,13 @@ fun androidFeastApiModule(
     baseUrl: String,
     ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ): Module = module {
-    includes(androidGraphQlModule(GraphQlConfig(baseUrl = baseUrl), ioDispatcher))
+    includes(graphQlModule(GraphQlConfig(baseUrl = baseUrl), ioDispatcher))
     single<GraphQLRepository> { GraphQlRepositoryImpl(get()) }
 }
 
+@Suppress("UNUSED_PARAMETER")
 fun androidFeastApiModule(
     context: Context,
     baseUrl: String,
-): Module = module {
-    includes(androidGraphQlModule(context, GraphQlConfig(baseUrl = baseUrl)))
-    single<GraphQLRepository> { GraphQlRepositoryImpl(get()) }
-}
+): Module = androidFeastApiModule(baseUrl)
 
