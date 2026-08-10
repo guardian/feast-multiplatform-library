e916918a9cfa9c82507658099f26c49acbfe5d32
Mohammad Haque <mohammad.haque@guardian.co.uk>
2026-08-10 17:38:54 +0100

    Added new ingredient field to support guidance feature plus some cleanup and refactoring

--- library/src/commonTest/kotlin/com/gu/recipe/RenderRecipeTest.kt
+++ library/src/commonTest/kotlin/com/gu/recipe/RenderRecipeTest.kt
@@ -809,7 +809,7 @@ class RenderRecipeTest {
             ingredients = listOf(
                 IngredientsList(
                     ingredientsList = listOf(
-                        IngredientItem(text = "1 eggplant")
+                        IngredientItem(text = "1 eggplant", convertedTerm = "eggplant")
                     )
                 )
             )
@@ -918,8 +918,8 @@ class RenderRecipeTest {
         assertEquals("The eggplant curry needs to be hot and tangy. We need green chillies and soaked tamarind.", rendered.description)
         assertEquals("1 <u>eggplant</u>", ingredient?.text)
         assertEquals("medium size eggplants", ingredient?.template)
-        assertEquals("<strong>aubergine</strong>\n\nThis is uk guidance notes for aubergine", ingredient?.ukGuidance)
-        assertEquals("<strong>eggplant</strong>\n\nThis is us guidance notes for eggplant", ingredient?.usGuidance)
+        assertEquals("This is uk guidance notes for aubergine", ingredient?.ukGuidance)
+        assertEquals("This is us guidance notes for eggplant", ingredient?.usGuidance)
         assertEquals("Cut eggplants meanwhile soak tamarind in a water", rendered.instructions?.get(0)?.description)
         assertEquals("Get portion of eggplant curry in a serving plate", rendered.instructions?.get(1)?.description)
     }
@@ -976,12 +976,9 @@ class RenderRecipeTest {
         assertEquals("The eggplant curry needs to be hot and tangy. We need green chillies and soaked tamarind.", rendered.description)
         assertEquals("1 <u>eggplant</u>", ingredient?.text)
         assertEquals("medium size eggplants", ingredient?.template)
-        assertEquals("<strong>aubergine</strong>\n\nThis is uk guidance notes for aubergine", ingredient?.ukGuidance)
-        assertEquals("<strong>eggplant</strong>\n\nThis is us guidance notes for eggplant", ingredient?.usGuidance)
+        assertEquals("This is uk guidance notes for aubergine", ingredient?.ukGuidance)
+        assertEquals("This is us guidance notes for eggplant", ingredient?.usGuidance)
         assertEquals("Cut eggplants meanwhile soak tamarind in a water", rendered.instructions?.get(0)?.description)
         assertEquals("Get portion of eggplant curry in a serving plate", rendered.instructions?.get(1)?.description)
     }
-
-
-
 }
